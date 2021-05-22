9095aa7d500e74cb6b8f8c6b746844095250bd27
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-05-22 15:54:21 -0400

    Added Documentation for addDigit method

--- app/src/main/java/com/segg3/calculator/Calculator.java
+++ app/src/main/java/com/segg3/calculator/Calculator.java
@@ -16,15 +16,25 @@ public class Calculator {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * addDigit is a method to add a digit to the end of the previous number or to create a new number in TokenList
+     *
+     * @param digit is a char that will be either appended to the end of the last token number or add new number to the tokenList
+     *
+     * @exception IllegalArgumentException is thrown when digit is not a number or a period
+     *
+     */
     public void addDigit(char digit)
     {
-        Token last = tokenList.get(tokenList.size() - 1);
-
+        //checking for invalid inputs
         if(!char.isDigit(digit) && digit != '.')
         {
             throw new IllegalArgumentException("digit char needs to be a number or a '.'");
         }
 
+        Token last = tokenList.get(tokenList.size() - 1);
+
+        //if last token is a number then the digit will be appended to the existing number
         if(last.getTokenType() == TokenType.Number)
         {
             if(char.isDigit(digit) || (digit == '.' && !last.data.contains(".")))
@@ -32,6 +42,7 @@ public class Calculator {
                 last.data = last.data + digit.toString();
             }
         }
+        //else new token is added to the end of token list
         else
         {
             if(char.isDigit(digit))
@@ -43,7 +54,6 @@ public class Calculator {
                 tokenList.add(TokenType.Number, "0.");
             }
         }
-
     }
 
     public void addOperations(String str)
