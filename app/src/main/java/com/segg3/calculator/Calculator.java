affe54370003c30b311a2c87f342de9458eca009
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-05-22 10:51:37 -0400

    Created Calculator class

---
+++ app/src/main/java/com/segg3/calculator/Calculator.java
@@ -0,0 +1,28 @@
+package com.segg3.calculator;
+
+import com.segg3.calculator.tokenizer.TokenList;
+
+public class Calculator {
+
+    private TokenList tokenList;
+
+    public Calculator()
+    {
+        tokenList = new TokenList();
+    }
+
+    public float calculate()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public void addNum(String str)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public void addOperations(String str)
+    {
+        throw new UnsupportedOperationException();
+    }
+}
