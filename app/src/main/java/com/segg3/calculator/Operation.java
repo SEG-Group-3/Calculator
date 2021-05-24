0ce458d992c9fc146f6a5740f443d30f56d82359
kareem <kqara103@uottawa.ca>
2021-05-25 01:29:06 +0300

    Add calculate function and getRPN.

---
+++ app/src/main/java/com/segg3/calculator/Operation.java
@@ -0,0 +1,16 @@
+
+package com.segg3.calculator;
+
+public class Operation {
+    public final String identifier;
+    public final int priority;
+    public final int inputCount;
+    public final Oplmplementation implementation;
+
+    public Operation(String identifier, int priority, int inputCount, Oplmplementation implementation) {
+        this.identifier = identifier;
+        this.priority = priority;
+        this.inputCount = inputCount;
+        this.implementation = implementation;
+    }
+}
---
+++ app/src/main/java/com/segg3/calculator/Oplmplementation.java
@@ -0,0 +1,7 @@
+package com.segg3.calculator;
+import java.util.List;
+
+public interface Oplmplementation{
+        float op(List<Float> a);
+    }
+
